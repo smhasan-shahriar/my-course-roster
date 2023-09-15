9036fa705b3051d5bda7f84c1656a3c318e0c0a5
Ador Shahriar <ador.shahriar@gmail.com>
2023-09-15 10:09:55 +0600

    updated texts following design

--- src/components/Selections/Selections.jsx
+++ src/components/Selections/Selections.jsx
@@ -4,7 +4,7 @@ const Selections = ({selects, cost, hours, remaining}) => {
     return (
         
         <div className="lg:ml-6 mx-auto md:w-1/2 lg:w-1/4 h-full bg-white rounded-xl p-6 mb-10">
-            <h2 className='text-[#2F80ED] text-lg font-bold mb-4'>Credit Hour Remaining: {remaining} hrs</h2>
+            <h2 className='text-[#2F80ED] text-lg font-bold mb-4'>Credit Hour Remaining: {remaining} hr</h2>
             <hr />
             <h2 className='mt-4 mb-5 text-xl font-bold'>Course Name</h2>
             <ol className='mb-6'>
@@ -13,7 +13,7 @@ const Selections = ({selects, cost, hours, remaining}) => {
                 }
             </ol>
             <hr />
-            <h4 className='my-4 opacity-80 font-medium'>Total Credit Hour: {hours} hrs</h4>
+            <h4 className='my-4 opacity-80 font-medium'>Total Credit Hour: {hours}</h4>
             <hr />
             <h3 className='mt-4 mb-6 opacity-80 font-semibold'>Total Price: {cost} USD</h3>
         </div>
