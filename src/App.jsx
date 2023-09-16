aec4eff2c4d78257409c84130f193f9ab4916ce2
Ador Shahriar <ador.shahriar@gmail.com>
2023-09-16 08:57:17 +0600

    total credit hour toast message updated again

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       return;
     }
     if(creditHour > 20){
-      toast('20 credit hour limit will exceed. So this course cannot be selected.')
+      toast('20 credit hour limit will exceed if you select this course. So this course cannot be selected.')
     }
     let remainingHour = 20 - course.credit;
     newSelects.map(course => remainingHour -= course.credit);
